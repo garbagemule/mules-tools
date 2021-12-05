d1f3963e58994ba632133658620884be70516979
Andreas Troelsen <garbagemule@gmail.com>
2021-12-05 15:21:52 +0100

    Remove unused Fragment import in WaveAnalyzer.

--- src/pages/WaveAnalyzer/WaveAnalyzer.jsx
+++ src/pages/WaveAnalyzer/WaveAnalyzer.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import yaml from 'js-yaml'
 
 import SplitPane from '../../components/SplitPane'
